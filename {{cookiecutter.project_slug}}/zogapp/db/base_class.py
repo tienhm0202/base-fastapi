0e2d255a9a99b881bc7053a9fbe0091de29992a2
Hoang Manh Tien <tienhm.0202@gmail.com>
2020-10-19 11:33:59 +0700

    chore: add db constraint naming convention

--- {{cookiecutter.project_slug}}/zogapp/db/base_class.py
+++ {{cookiecutter.project_slug}}/zogapp/db/base_class.py
@@ -4,13 +4,24 @@ from typing import Any, Dict, Generic, List, Optional, Type, TypeVar, Union
 import pytz
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
+from sqlalchemy import MetaData
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
 from sqlalchemy.orm import Session
 
 from zogapp import settings
 
+convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
 
-@as_declarative()
+metadata = MetaData(naming_convention=convention)
+
+
+@as_declarative(metadata=metadata)
 class Base:
     id: Any
     created_at: datetime
