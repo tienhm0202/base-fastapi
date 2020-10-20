234cf5c302d44d033bc9202be0951684ae0d6259
Hoang Manh Tien <tienhm.0202@gmail.com>
2020-10-20 17:44:41 +0700

    chore: add nginx config to deploy

--- {{cookiecutter.project_slug}}/entrypoint.sh
+++ {{cookiecutter.project_slug}}/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 env PYTHONPATH=. alembic upgrade head
-gunicorn main:app -w 4 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:5000
+gunicorn main:app -w 4 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:{{cookiecutter.server_port}}
