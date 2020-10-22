9296ca2bbb7a03bf6c9fd838dc14088861fb36b3
Hoang Manh Tien <tienhm.0202@gmail.com>
2020-10-22 12:15:57 +0700

    chore: fix missing commit

--- {{cookiecutter.project_slug}}/entrypoint.sh
+++ {{cookiecutter.project_slug}}/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 env PYTHONPATH=. alembic upgrade head
-gunicorn main:app -w 4 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:{{cookiecutter.server_port}}
+gunicorn main:app -w 1 -k uvicorn.workers.UvicornWorker --access-logfile - -b 0.0.0.0:{{cookiecutter.server_port}}
