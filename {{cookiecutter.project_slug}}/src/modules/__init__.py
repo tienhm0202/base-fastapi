dd8a6f63c4b8126dfb071076348ab4d0ca153e1a
tienhm <tienhm.0202@gmail.com>
2021-03-30 22:58:24 +0700

    chore: add modile tags and prefix

--- {{cookiecutter.project_slug}}/src/modules/__init__.py
+++ {{cookiecutter.project_slug}}/src/modules/__init__.py
@@ -9,6 +9,10 @@ load_modules = []
 def init_app(app: FastAPI):
     router = APIRouter()
     for module in load_modules:
-        router.include_router(module.controllers.router)  # noqa
+        router.include_router(
+            module.controllers.router,
+            prefix=module.API_PREFIX,
+            tags=module.API_TAGS
+        )  # noqa
 
     app.include_router(router, prefix=settings.API_V1_STR)
