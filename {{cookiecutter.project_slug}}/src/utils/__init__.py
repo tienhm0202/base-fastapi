05d3c3809e65145ea87082d64471a752a0e16d1f
tienhm <tienhm.0202@gmail.com>
2021-04-03 15:37:01 +0700

    chore: add get_logger with level to utils

--- {{cookiecutter.project_slug}}/src/utils/__init__.py
+++ {{cookiecutter.project_slug}}/src/utils/__init__.py
@@ -1,3 +1,6 @@
+import logging
+
+from zogutils.middlewares import logger
 from zogutils.secret import unique_id
 
 
@@ -7,3 +10,12 @@ def get_db_id() -> str:
 
 def get_secret() -> str:
     return unique_id(32, "KEY_")
+
+
+def get_logger(name: str):
+    level = logging.getLevelName(settings.LOG_LEVEL)
+
+    new_logger = logger.get_logger(name)
+    new_logger.setLevel(level)
+
+    return new_logger
